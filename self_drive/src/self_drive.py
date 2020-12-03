0c2e8faf2c7fa0bdd465d5d5ca46d65d0852e8db
jh79783 <jh79783@naver.com>
2020-12-03 13:55:03 +0900

    front turn

--- self_drive/src/self_drive.py
+++ self_drive/src/self_drive.py
@@ -7,18 +7,40 @@ from sensor_msgs.msg import LaserScan
 class SelfDrive:
     def __init__(self, publisher):
         self.publisher = publisher
-        self.count = 30
 
     def lds_callback(self, scan):
         turtle_vel = Twist()
-        rospy.loginfo(scan.ranges[0])
-        if scan.ranges[0] < 0.15:
-            turtle_vel.linear.x = 0.0
+        forward_l = self.avg_distance(scan.ranges[350:359])
+        forward_r = self.avg_distance(scan.ranges[0:10])
+        forward = self.avg_distance([forward_l, forward_r])
+        if (forward < 0.3) and (forward > 0):
+            turtle_vel.linear.x = 0.1
+            turtle_vel.angular.z = -1.5
            
         else:
-            turtle_vel.linear.x = 0.1
-        turtle_vel.angular.z = 0.0
+            turtle_vel.linear.x = 0.18
+            turtle_vel.angular.z = 0
         self.publisher.publish(turtle_vel)
+
+    def avg_distance(self, scan):
+        count = 0
+        distance = 0
+
+        for i in scan:
+            if i != 0:
+                count += 1
+                distance += i
+
+        if count != 0:
+            avg_dis = distance/count
+        else:
+            avg_dis = 3
+
+        if avg_dis != 0:
+            self.temp = avg_dis
+        if avg_dis == 0 :
+            avg_dis = self.temp
+        return avg_dis
 
 def main():
     rospy.init_node('self_drive')
