a66dcfa615c2bf910c5cd50fb582e077fbe2a69a
jh79783 <jh79783@naver.com>
2020-12-03 14:06:52 +0900

    straight & middle lidar

--- self_drive/src/self_drive.py
+++ self_drive/src/self_drive.py
@@ -4,17 +4,23 @@ import rospy
 from geometry_msgs.msg import Twist
 from sensor_msgs.msg import LaserScan
 
+
 class SelfDrive:
     def __init__(self, publisher):
         self.publisher = publisher
+        self.temp = 0
 
     def lds_callback(self, scan):
         forward_l = self.avg_distance(scan.ranges[350:359])
         forward_r = self.avg_distance(scan.ranges[0:10])
         forward = self.avg_distance([forward_l, forward_r])
         front_side = self.avg_distance(scan.ranges[30:50])
+        middle = self.avg_distance(scan.ranges[85:95])
+
         if (forward < 0.3) and (forward > 0):
             self.go_turn(0.1, -1.5)
+        elif (forward >= 0.3) and (middle > 0.9):
+            self.go_turn(0.18, 0)
            
         else:
             if (front_side < 0.25) and (front_side > 0):
@@ -23,6 +29,7 @@ class SelfDrive:
                 self.go_turn(0.15, 0.5)
             else:
                 self.go_turn(0.18, 0)
+
     def avg_distance(self, scan):
         count = 0
         distance = 0
@@ -49,6 +56,7 @@ class SelfDrive:
             turtle_vel.angular.z = z
             self.publisher.publish(turtle_vel)
 
+
 def main():
     rospy.init_node('self_drive')
     publisher = rospy.Publisher('cmd_vel', Twist, queue_size=1)
@@ -56,6 +64,7 @@ def main():
     subscriber = rospy.Subscriber('scan', LaserScan,
                                   lambda scan: driver.lds_callback(scan))
     rospy.spin()
+
 
 if __name__ == "__main__":
     main()
